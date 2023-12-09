020b4440421f11c1606261cc015f007305b025a0
sunith88 <kss.sunithsenthil68@gmail.com>
2023-12-09 16:06:10 +0530

    dadhboard template added

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,13 @@
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {  
+  return (
+    <main className="grow">
+
+      {children}
+
+    </main>
+  )
+}
